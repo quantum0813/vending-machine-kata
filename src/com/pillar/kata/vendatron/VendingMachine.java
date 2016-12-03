e4044645b1991dcffd17b8d17a26554e222e2272
quantum0813 <quantum0813@gmail.com>
2016-12-02 23:55:22 -0500

    First commit

---
+++ src/com/pillar/kata/vendatron/VendingMachine.java
@@ -0,0 +1,7 @@
+package com.pillar.kata.vendatron;
+
+/**
+ * Created by eric on 12/2/16.
+ */
+public class VendingMachine {
+}
---
+++ test/VendingMachineTest.java
@@ -0,0 +1,11 @@
+import com.pillar.kata.vendatron.VendingMachine;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VendingMachineTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
